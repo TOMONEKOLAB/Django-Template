f94262088ce4a94bd981ae9be1fd34accf2618b2
TOMONEKO <tomoneko0831@gmail.com>
2025-06-16 01:06:00 +0900

    Django-Template

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -ex
+
+if [ -f /workspaces/.gitkeep ]; then
+    echo "Remove .gitkeep..."
+    rm /workspaces/.gitkeep
+fi
+
+case $CONTAINER in
+    Django-app)
+        if [ ! -f /workspaces/manage.py ]; then
+            echo "Creating new Django project..."
+            django-admin startproject config .
+        else
+            echo "Starting Django Project..."
+            if [ "$DEBUG" = "True" ]; then
+                exec python manage.py runserver 0.0.0.0:8080
+            else
+                exec gunicorn config.wsgi:application --bind 0.0.0.0:8080
+            fi
+        fi
+        ;;
+    Django-db)
+        chown -R postgres:postgres /workspaces
+        if [ ! -s "/workspaces/PG_VERSION" ]; then
+            echo "Initializing PostgreSQL database..."
+            su - postgres -c "/usr/lib/postgresql/17/bin/initdb -D /workspaces"
+            echo "listen_addresses = '*'" >> /workspaces/postgresql.conf
+            echo "port = 5432" >> /workspaces/postgresql.conf
+            echo "host    all             all             0.0.0.0/0               trust" >> /workspaces/pg_hba.conf
+        fi
+        echo "Starting PostgreSQL..."
+        exec su - postgres -c "/usr/lib/postgresql/17/bin/postgres -D /workspaces"
+        ;;
+    Django-web)
+        echo "Starting Nginx..."
+        exec nginx -g 'daemon off;'
+        ;;
+    *)
+        exec "$@"
+        ;;
+esac
