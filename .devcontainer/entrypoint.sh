a43b3fed82c27942cb984d5b50cb3180099f7800
TOMONEKOLAB <tomoneko0831@gmail.com>
2025-06-20 04:27:25 +0900

    Update entrypoint.sh

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -23,17 +23,24 @@ case $CONTAINER in
         ;;
     Django-db)
         chown -R postgres:postgres /workspaces
+
         if [ ! -s "/workspaces/PG_VERSION" ]; then
-            echo "Initializing PostgreSQL database..."
+            echo "Initializing PostgreSQL database cluster..."
             su - postgres -c "/usr/lib/postgresql/17/bin/initdb -D /workspaces"
-            su - postgres -c "/usr/lib/postgresql/17/bin/pg_ctl -D /workspaces -w start"
-            su - postgres -c "createdb $POSTGRES_DB"
             echo "listen_addresses = '*'" >> /workspaces/postgresql.conf
             echo "port = 5432" >> /workspaces/postgresql.conf
-            echo "host    all             all             0.0.0.0/0               trust" >> /workspaces/pg_hba.conf
+            echo "host all all 0.0.0.0/0 trust" >> /workspaces/pg_hba.conf
+            su - postgres -c "/usr/lib/postgresql/17/bin/pg_ctl -D /workspaces -w start"
+            if [ "$POSTGRES_USER" != "postgres" ]; then
+                su - postgres -c "createuser $POSTGRES_USER"
+            fi
+            su - postgres -c "psql -c \"ALTER USER \\\"$POSTGRES_USER\\\" WITH PASSWORD '$POSTGRES_PASSWORD';\""
+            su - postgres -c "createdb -O $POSTGRES_USER $POSTGRES_DB"
+            su - postgres -c "/usr/lib/postgresql/17/bin/pg_ctl -D /workspaces -m fast -w stop"
         fi
+
         echo "Starting PostgreSQL..."
-        touch .gitkeep
+        touch /workspaces/.gitkeep
         exec su - postgres -c "/usr/lib/postgresql/17/bin/postgres -D /workspaces"
         ;;
     Django-web)
